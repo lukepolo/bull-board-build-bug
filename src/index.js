66552b44d2700ec7cb6abd212769d315c81ca237
Luke Policinski <luke@lukepolo.com>
2021-06-16 09:54:11 -0400

    show that bullmq works

--- src/index.js
+++ src/index.js
@@ -1,22 +1,26 @@
-const express = require('express')
-const QueueMQ = require('bullmq')
-const { createBullBoard } = require('bull-board')
-const { BullMQAdapter } = require('bull-board/bullMQAdapter')
+const express = require("express");
+const bullmq = require("bullmq");
+const { createBullBoard } = require("bull-board");
+const { BullMQAdapter } = require("bull-board/bullMQAdapter");
 
-const queueMQ = new QueueMQ.Queue('default')
-const { router } = createBullBoard([
-  new BullMQAdapter(queueMQ),
-])
-const app = express()
+const queueMQ = new bullmq.Queue("default");
+const { router } = createBullBoard([new BullMQAdapter(queueMQ)]);
+const app = express();
 
-app.use('/admin/queues', router)
+app.use("/admin/queues", router);
 
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+queueMQ.add("hello", { data: "world" });
+
+new bullmq.Worker("default", async (job) => {
+  console.info("PROCESSING", job.name);
+});
